1c31d649d250bd76619fb855e68f6a82f04f64fc
Pedro Gomes <pdcgomes@gmail.com>
2015-09-16 12:06:57 -0400

    [ADD] ChangeReportRenderer stubs [ADD] ChangeTracker stub

---
+++ RendezVous/ChangeReportJSONRenderer.swift
@@ -0,0 +1,9 @@
+//
+//  ChangeReportJSONRenderer.swift
+//  RendezVous
+//
+//  Created by Pedro Gomes on 16/09/2015.
+//  Copyright © 2015 Pedro Gomes. All rights reserved.
+//
+
+import Foundation
---
+++ RendezVous/ChangeReportRenderer.swift
@@ -0,0 +1,9 @@
+//
+//  ChangeReportRenderer.swift
+//  RendezVous
+//
+//  Created by Pedro Gomes on 15/09/2015.
+//  Copyright © 2015 Pedro Gomes. All rights reserved.
+//
+
+import Foundation
